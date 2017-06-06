3a626c82164384b75481e4875592c875daa6bcd6
Luiz Guilherme <luizg@takenet.com.br>
2017-06-06 14:44:24 -0300

    Refactor notification service

--- sdk/src/NotificationService.js
+++ sdk/src/NotificationService.js
@@ -1,18 +1,19 @@
 export class NotificationService {
 
     constructor() {
+        this.notificationBlinkInterval = 4000;
         this.pageTitle = window.document.title;
+        this.botName;
         this.notificationTimer;
         window.document.addEventListener('visibilitychange', this._onVisibilityChange);
     }
 
     _onVisibilityChange() {
-        if (window.document.hidden) {
+        if (window.document.hidden) {//if tab is being hidden
             return;
         }
 
-        if (this.notificationTimer)
-            clearTimeout(this.notificationTimer);
+        this._clearTimer()
 
         if (this.pageTitle) {
             this._changeWindowTitle(this.pageTitle);
@@ -24,18 +25,26 @@ export class NotificationService {
         if (window.document.hidden) {
             var unreadMsgNot = unreadMessagesCount > 0 ? "(" + unreadMessagesCount + ") " : "";
             let notificationTitle = unreadMsgNot + botName + " diz...";
+            this._clearTimer();
             this._showNotification(notificationTitle);
-            this.notificationTimer = setInterval( () => this._showNotification(notificationTitle), 3000);
+            this.notificationTimer = setInterval(() => this._showNotification(notificationTitle), this.notificationBlinkInterval);
         }
     }
 
     _showNotification(notificationTitle) {
         this._changeWindowTitle(notificationTitle);
-        setTimeout( () => this._changeWindowTitle(this.pageTitle), 1500);
+        setTimeout(() => this._changeWindowTitle(this.pageTitle), this.notificationBlinkInterval / 2);
     }
 
     _changeWindowTitle(title) {
         window.document.title = title;
+    }
+
+    _clearTimer() {
+        if (this.notificationTimer) {
+            clearTimeout(this.notificationTimer);
+            this.notificationTimer = undefined;
+        }
     }
 }
 
